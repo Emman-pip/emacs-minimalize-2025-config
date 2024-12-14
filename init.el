bb7b76abe588a298b5ae89d28d4a8ffd0b01b348
Emman-pip <parrenoemmanuel756@gmail.com>
2024-12-14 22:41:31 +0800

    Remove the ugly defaults of emacs.

---
+++ init.el
@@ -0,0 +1,26 @@
+;; to remove the ugly defaults
+(menu-bar-mode 0)
+(tool-bar-mode 0)
+(scroll-bar-mode 0)
+
+(load-theme 'wombat)
+
+(set-face-attribute 'default nil :height 160)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(inhibit-default-init t)
+ '(inhibit-startup-buffer-menu t)
+ '(inhibit-startup-echo-area-message "Emman-pip")
+ '(inhibit-startup-screen t)
+ '(initial-buffer-choice nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
