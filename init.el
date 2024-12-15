2f37d4bcc0dc5c3e1f10bbab1e859fcbffa2d2bf
Emman-pip <parrenoemmanuel756@gmail.com>
2024-12-15 10:29:06 +0800

    Add melpa to sources.

--- init.el
+++ init.el
@@ -35,4 +35,12 @@
 (global-set-key "\M-n" 'flymake-goto-next-error)
 (global-set-key "\M-p" 'flymake-goto-prev-error)
 
+;; melpa
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
+;; and `package-pinned-packages`. Most users will not need or want to do this.
+;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(package-initialize)
+
 
