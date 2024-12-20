6cbae1c0710358959dd2a2731baf6929aaf9bf0c
Emman-pip <parrenoemmanuel756@gmail.com>
2024-12-20 10:36:31 +0800

    Seperate the custom variables to custom_vars.el.

---
+++ custom_vars.el
@@ -0,0 +1,19 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("72ed8b6bffe0bfa8d097810649fd57d2b598deef47c992920aef8b5d9599eefe" "2ff9ac386eac4dffd77a33e93b0c8236bb376c5a5df62e36d4bfa821d56e4e20" default))
+ '(inhibit-default-init t)
+ '(inhibit-startup-buffer-menu t)
+ '(inhibit-startup-echo-area-message "Emman-pip")
+ '(org-babel-load-languages '((lisp . t) (python . t)))
+ '(package-selected-packages
+   '(evil-mode all-the-icons dashboard golden-ratio evil-visual-mark-mode which-key web-mode elpy gruvbox-theme)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
