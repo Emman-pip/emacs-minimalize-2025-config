216cfd6a64cedcb5a02c05224af702b0001ed09f
Emman-pip <parrenoemmanuel756@gmail.com>
2025-01-09 10:53:36 +0800

    Minor changes (theme, WM, ace-window).
    Change theme to modus vivendi.
    Remove EXWM.
    add ace window.

--- custom_vars.el
+++ custom_vars.el
@@ -3,14 +3,16 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(backup-directory-alist '(("**" . "~/.emacs.d/tmp")))
  '(custom-safe-themes
-   '("72ed8b6bffe0bfa8d097810649fd57d2b598deef47c992920aef8b5d9599eefe" "2ff9ac386eac4dffd77a33e93b0c8236bb376c5a5df62e36d4bfa821d56e4e20" default))
+   '("937401a2e532f2c8c881b6b3f20d9d4b6b9405bccf72ea6289c9d3f4507eb1ab" "a75aff58f0d5bbf230e5d1a02169ac2fbf45c930f816f3a21563304d5140d245" "7b602fe4a324dc18877dde647eb6f2ff9352566ce16d0b888bfcb870d0abfd70" "2e7dc2838b7941ab9cabaa3b6793286e5134f583c04bde2fba2f4e20f2617cf7" "72ed8b6bffe0bfa8d097810649fd57d2b598deef47c992920aef8b5d9599eefe" "2ff9ac386eac4dffd77a33e93b0c8236bb376c5a5df62e36d4bfa821d56e4e20" default))
+ '(evil-want-C-u-scroll t)
  '(inhibit-default-init t)
  '(inhibit-startup-buffer-menu t)
  '(inhibit-startup-echo-area-message "Emman-pip")
  '(org-babel-load-languages '((lisp . t) (python . t)))
  '(package-selected-packages
-   '(evil-mode all-the-icons dashboard golden-ratio evil-visual-mark-mode which-key web-mode elpy gruvbox-theme)))
+   '(modus-themes magit lsp-ui tree-sitter-langs tree-sitter ace-window smartparens general lsp-mode php-mode flymake-php company-phpactor phpactor prettier emmet-mode multiple-cursors jade-mode pug-mode multi-vterm vterm evil-mode all-the-icons dashboard golden-ratio evil-visual-mark-mode which-key web-mode elpy gruvbox-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
